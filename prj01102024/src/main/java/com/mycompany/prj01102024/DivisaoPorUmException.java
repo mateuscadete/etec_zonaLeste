adfd99d17c2c6afd9de70b66cfa54bbabbdb6d65
Mateus Cadete <91164291+mateuscadete@users.noreply.github.com>
2024-10-01 22:23:04 -0300

    Commitando alterações para a entrega da atividade

---
+++ prj01102024/src/main/java/com/mycompany/prj01102024/DivisaoPorUmException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prj01102024;
+
+/**
+ *
+ * @author Admin
+ */
+public class DivisaoPorUmException extends RuntimeException{
+    public DivisaoPorUmException(String s){
+        super(s);
+    
+    }
+}
